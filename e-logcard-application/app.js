bdfe895c7a7516de830de9ee3db5bd5955df66a4
N'DA <damien.n-da@capgemini.com>
2017-05-05 15:58:11 +0200

    security-jwt

--- e-logcard-application/app.js
+++ e-logcard-application/app.js
@@ -9,8 +9,6 @@ let hfcUtil=require("./custom_node_modules/utils/hfcUtil.js");
 let database=require("./custom_node_modules/database/mongo.js");
 let logcardManager=require("./custom_node_modules/express-middlewares/routers/logcard-management.js");
 
-/*let bodyParser = require('body-parser');
-*/
 
 //APPLICATION
 
@@ -23,8 +21,9 @@ database.init()
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: false }));
 	
-	let sessionManager=require("./custom_node_modules/express-middlewares/security/session-manager.js")(app);
-	app.use(sessionManager);
+	let tokenManager=require("./custom_node_modules/express-middlewares/security/token-manager.js")
+	app.use(tokenManager.jwtMiddleware);
+	
 	app.use(viewRouter);
 	app.use(userManagement.router);
 	app.use("/logcard",logcardManager.router);
@@ -35,4 +34,3 @@ database.init()
 	});
 	
 });
-//DAMS
